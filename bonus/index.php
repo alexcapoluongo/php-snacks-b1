587084b1dfdf1810773a70942995b4e16013be14
alexcapoluongo <alexcapoluongo@icloud.com>
2022-06-08 16:16:39 +0200

    added bonus

--- bonus/index.php
+++ bonus/index.php
@@ -1,2 +1,10 @@
 <!-- ## Snack 4 (Bonus)
-Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta --> -->
+Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta --> 
+
+<?php
+$random_array = range(0, 100); //choose a range 
+shuffle($random_array); //randomize it
+$random_array = array_slice($random_array ,0,10); //give a length to an array 
+var_dump($random_array)
+?>
+
